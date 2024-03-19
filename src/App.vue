d0b681489fae04701d8d222767956da83b7a979d
JuanTobon7 <jctobon11.2@gmail.com>
2024-03-19 12:32:22 -0500

    cambio

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <navbar></navbar>
   <main class="pt-20 min-h-[80vh]">    
-    <offerAdd></offerAdd>
     <RouterView></RouterView>
   </main>
   <defaultFooter></defaultFooter>
@@ -11,6 +10,5 @@
 import { useRouter } from 'vue-router'
 import navbar from './components/NavBar.vue'
 import defaultFooter from './components/Footer.vue'
-import offerAdd from './components/OfferAdd.vue'
 
 </script>
--- src/views/Actividades.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <section class="w-full h-[80vh]">
-        <div class="container mx-auto">
-            <h1>tenemos un amplio cronograma</h1>
-        </div>
-    </section>
-</template>
