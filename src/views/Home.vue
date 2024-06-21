64502329ced221af3504df85e0497aaf59950bc4
JuanTobon7 <jctobon11.2@gmail.com>
2024-06-21 15:21:47 -0500

    back-front connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,30 +2,33 @@
   <h1>HOla</h1>
   <section class="container">
     <div class="flex justify-items-center py-4 bg-primary-300 w-[30vh]">
-      <form @submit.prevent="singInMethod" class="p-5 flex flex-col justify-items-center gap-2">
+      <form @submit.prevent="registerMethod" class="p-5 flex flex-col justify-items-center gap-2">
         <h2>Inicia sesión</h2>
-        <input v-model="name" class="border-primary-500 rounded-md hover:border-primary-900 hover:scale-105 transition-transform px-2" placeholder="Nombre">
+        <input v-model="email" class="border-primary-500 rounded-md hover:border-primary-900 hover:scale-105 transition-transform px-2" placeholder="Email">
+        <input v-model="password" class="border-primary-500 rounded-md hover:border-primary-900 hover:scale-105 transition-transform px-2" placeholder="Password">
         <button type="submit" class="rounded-md bg-primary-400 text-primary-900">Enviar</button>
       </form>
     </div>
   </section>
 </template>
 <script>
-import {singIn} from '../api/router'
+import {resgiter} from '../api/router'
 
 export default {
   data() {
     return {
-      name: null,
+      email: null,
+      password: null,
       message: null
     }
   },
   methods: {
-    async singInMethod(){
-        const name = this.name;
-        if (!name) return;
+    async registerMethod(){
+        const email = this.email;
+        const password = this.password;
+        if (!email || !password) return;
         try {
-            const response = await singIn(name);
+            const response = await resgiter({email,password});
             this.message = response.data; 
         } catch (error) {
             console.error('Error al iniciar sesión:', error);
